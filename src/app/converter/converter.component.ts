357d9a2e50d22d4366db8ebce076d048d6c93d28
Narinder229 <narinder29@gmail.com>
2025-12-13 16:23:37 +0530

    GA4 and GTM integration

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -14,15 +14,34 @@ export class ConverterComponent {
   inputText = '';
   statusMessage = '';
 
+   
   constructor(private converter: ConversionService) {}
 
   convertJson() {
+    window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: 'convert_json_excel',
+    tool: 'json to excel',
+    action: 'convert'
+  });
     const success = this.converter.jsonToExcel(this.inputText, 'json_output');
     this.statusMessage = success ? 'JSON converted successfully!' : 'Invalid JSON format.';
   }
 
   convertXml() {
+    window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: 'convert_xml_excel',
+    tool: 'xml to excel',
+    action: 'convert'
+  });
     const success = this.converter.xmlToExcel(this.inputText, 'xml_output');
     this.statusMessage = success ? 'XML converted successfully!' : 'Invalid XML format.';
+  }
+}
+
+declare global {
+  interface Window {
+    dataLayer: any[];
   }
 }
